354b62af3f580f0486785e3da6ba9d1a3a80e018
Hekayaty-MO <hekayaty29@gmail.com>
2026-02-06 20:10:47 +0200

    Implement Forgot Password system and fix import error

--- client/src/pages/AuthPage.tsx
+++ client/src/pages/AuthPage.tsx
@@ -1,6 +1,6 @@
 
 import { useState, useEffect } from "react";
-import { useLocation } from "wouter";
+import { useLocation, Link } from "wouter";
 import { useAuth } from "@/hooks/use-auth";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
